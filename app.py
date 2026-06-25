9996810e3f6982e456d0a2481bde92f77cf38a47
hooran-gallery <q1297504@gmail.com>
2026-06-25 11:37:45 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html", msg="سلام از Render")
+
 
 if __name__ == "__main__":
     app.run()
